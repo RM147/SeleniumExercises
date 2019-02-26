888fab00cd3b85f3b40fc6a294077b5b3d0b806b
Rana <mittrarana@gmail.com>
2019-02-26 17:12:02 +0000

    wait test done

--- Selenium/src/test/java/test/PaintTest.java
+++ Selenium/src/test/java/test/PaintTest.java
@@ -1,11 +1,20 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import pages.Painter;
 
@@ -23,7 +32,7 @@ public class PaintTest {
 	public void teardown() {
 		driver.quit();
 	}
-	
+	@Ignore
 	@Test
 	public void paintTest() throws InterruptedException {
 		driver.manage().window().maximize();
@@ -31,6 +40,27 @@ public class PaintTest {
 		Painter paint = PageFactory.initElements(driver, Painter.class);
 		paint.method(driver);
 		Thread.sleep(2000);
+	}
+	
+	@Test
+	public void waitTest() {
+		driver.manage().window().maximize();
+		driver.get("https://christophperrins.github.io/TestingSite/");		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebElement hatred = driver.findElement(By.xpath("//*[@id=\"quote\"]/h2"));
+		assertTrue(hatred.isDisplayed());
+		
+		
+	}
+	
+	@Test
+	public void waitTestEx() {
+		driver.manage().window().maximize();
+		driver.get("https://christophperrins.github.io/TestingSite/");		
+		WebElement hatred = (new WebDriverWait(driver,10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"quote\"]/h2")));
+		assertTrue(hatred.isDisplayed());
+		
 	}
 	
 	
